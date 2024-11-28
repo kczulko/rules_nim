42a0c88c5ebd27e51a2ed54c9def0081bb0b8c8f
kczulko <karol.czulkowski@protonmail.com>
2024-11-28 08:48:13 +0100

    fix: add --noSSLCheck to nimble + xcode in nix-shell

--- flake.nix
+++ flake.nix
@@ -11,33 +11,44 @@
         pkgs = import nixpkgs { inherit system; };
 
         wrap_bazelisk = { bazelisk, makeWrapper }:
-          bazelisk.overrideAttrs (prev: final: {
-            nativeBuildInputs = [ makeWrapper ] ++ final.nativeBuildInputs;
+          bazelisk.overrideAttrs (final: prev: {
+            nativeBuildInputs = [ makeWrapper ] ++ prev.nativeBuildInputs;
             postFixup = ''wrapProgram $out/bin/bazelisk \
               --unset TMP \
               --unset TMPDIR'';
           });
+
+        # possible fix for macos failure
+        # --noSSLCheck added
+        wrap_nimble = { nimble, makeWrapper }:
+          nimble.overrideAttrs (final: prev: {
+            nativeBuildInputs = [ makeWrapper ] ++ prev.nativeBuildInputs;
+            postFixup = ''wrapProgram $out/bin/nimble \
+              --add-flags --noSSLCheck
+            '';
+          });
+
+        nimble_wrapped = (pkgs.callPackage wrap_nimble {});
 
         fhsDefaultAttrs = {
           name = "simple-bazelisk-env";
           targetPkgs = pkgs: (with pkgs; [
             bash
             (pkgs.callPackage wrap_bazelisk {})
+            nimble_wrapped
             libz.dev
             gcc
             nim
-            nimble
           ]);
         };
-#        fhsCiAttrs = fhsDefaultAttrs // { runScript = "bazelisk test //e2e:all_integration_tests"; };
 
         shells = {
           default = (pkgs.buildFHSEnv fhsDefaultAttrs).env;
           ci = pkgs.mkShell {
             packages = with pkgs; [
               bazelisk
-              nimble
-            ];
+              nimble_wrapped
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs;[ darwin.xcode ]);
           };
         };
       in
